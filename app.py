3c5490a593e70cbf7016cb1d0aab7ccc6afb46e1
ArshakMkhoyan <arshakmkhoyanvb@gmail.com>
2020-09-01 23:01:15 +0500

    Changed the input to be a link

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
+import requests
 from skimage.color import rgb2lab, deltaE_cie76
 from flask import Flask, request, jsonify
-from PIL import Image
 import numpy as np
 import cv2
-import io
 
 app = Flask(__name__)
 
 
-def match_image_by_color(image, blue_color, green_color, threshold=10):
+def match_image_by_color(image, blue_color, green_color, threshold=15):
     selected_image = rgb2lab(np.uint8(np.asarray([image])))
     blue_color = rgb2lab(np.uint8(np.asarray([[blue_color]])))
     green_color = rgb2lab(np.uint8(np.asarray([[green_color]])))
@@ -20,7 +19,7 @@ def match_image_by_color(image, blue_color, green_color, threshold=10):
     mask = mask.reshape(mask.shape[1:])
 
     mask = mask.astype(np.uint8)
-    kernel = np.ones((9, 1), np.uint8)
+    kernel = np.ones((5, 1), np.uint8)
     mask = cv2.dilate(mask, kernel, iterations=2)
 
     return np.around(mask.mean(), 3), mask
@@ -28,18 +27,19 @@ def match_image_by_color(image, blue_color, green_color, threshold=10):
 
 @app.route("/nature_concentration/", methods=['POST'])
 def get_nature_concentration():
-    if request.files:
+    if request.json and 'image' in request.json:
         green_rgb = [201.0, 240.0, 187.0]
         blue_rgb = [162.0, 206.0, 255.0]
 
-        image_bytes = request.files['file'].read()
-        image = np.array(Image.open(io.BytesIO(image_bytes)))
+        image_bytes = requests.get(request.json['image']).content
+        image_bgr = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), -1)
+        image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
 
-        nature_concentration_value, image_mask = match_image_by_color(image, blue_rgb, green_rgb)
+        nature_concentration_value, image_mask = match_image_by_color(image_rgb, blue_rgb, green_rgb)
 
         return jsonify(nature_concentration_value=nature_concentration_value, image_mask=image_mask.tolist())
     else:
-        return None
+        return {'nature_concentration_value': None, 'image_mask': None}
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -3,14 +3,29 @@ import requests
 import numpy as np
 import matplotlib.pyplot as plt
 
-files = {'file': open('test_images/green.jpeg', 'rb')}
-response = requests.post('http://0.0.0.0:5000/nature_concentration', files=files)
-print(response.ok)
 
-response = response.json()
-print(response['nature_concentration_value'])
+def test(inp):
+    # files = {'file': open(inp, 'rb')}
+    # response = requests.post('http://0.0.0.0:5000/nature_concentration', files=files)
+    response = requests.post('http://0.0.0.0:5000/nature_concentration', json=inp)
+    print(response.ok)
 
-plt.imshow(response['image_mask'])
-plt.imsave('test_images/output.jpg', np.array(response['image_mask']),  cmap='gray')
-plt.show()
+    response = response.json()
+    print(response['nature_concentration_value'])
 
+    plt.imshow(response['image_mask'])
+    plt.imsave('test_images/output_new.jpg', np.array(response['image_mask']), cmap='gray')
+    plt.show()
+
+
+# image_bytes = request.files['file'].read()
+# image = np.array(Image.open(io.BytesIO(image_bytes)))
+
+# link = 'test_images/green.jpeg'
+# image_bytes = open(request.json['image'], 'rb').read()
+
+link = 'https://maps.googleapis.com/maps/api/staticmap?center=54.7829996,' \
+       '%209.4562149&zoom=8&size=640x400&maptype=roadmap&markers=color:red%7C54.7829996,' \
+       '%209.4562149&key=AIzaSyCf6NDAXlZ5E1PdqJrAqCqhxEOQ7vYhDWI'
+inp = {'image': link}
+test(inp)
